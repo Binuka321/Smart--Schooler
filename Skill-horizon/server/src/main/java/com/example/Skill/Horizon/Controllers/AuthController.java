fc3ff1875911fe644934e4cb31d5a2448533f52b
Binuka321 <binukaboss@gmail.com>
2025-04-04 09:12:02 +0530

    main changes

--- Skill-horizon/server/src/main/java/com/example/Skill/Horizon/Controllers/AuthController.java
+++ Skill-horizon/server/src/main/java/com/example/Skill/Horizon/Controllers/AuthController.java
@@ -15,7 +15,6 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 

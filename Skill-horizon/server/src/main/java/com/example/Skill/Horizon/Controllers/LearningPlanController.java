81de3e0a70192664f82cb6ca071a5b4c704ddcc6
Binuka321 <binukaboss@gmail.com>
2025-04-26 00:41:16 +0530

    bugfix in learning plans

--- Skill-horizon/server/src/main/java/com/example/Skill/Horizon/Controllers/LearningPlanController.java
+++ Skill-horizon/server/src/main/java/com/example/Skill/Horizon/Controllers/LearningPlanController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/plans")
-@CrossOrigin(origins = "*") 
+@CrossOrigin(origins = "http://localhost:5173") 
 
 public class LearningPlanController {
 

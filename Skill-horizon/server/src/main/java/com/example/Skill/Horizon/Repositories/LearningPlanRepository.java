fae3bbe6d310a491b3bbadbecbc17e9cf3d00f69
Binuka321 <binukaboss@gmail.com>
2025-04-25 15:39:10 +0530

    learning plan backend

---
+++ Skill-horizon/server/src/main/java/com/example/Skill/Horizon/Repositories/LearningPlanRepository.java
@@ -0,0 +1,10 @@
+package com.example.Skill.Horizon.Repositories;
+
+import com.example.Skill.Horizon.Models.LearningPlan;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+// Extend MongoRepository to use built-in CRUD operations
+public interface LearningPlanRepository extends MongoRepository<LearningPlan, String> {
+    // You can add custom queries if necessary, such as:
+    // List<LearningPlan> findByTitle(String title);
+}

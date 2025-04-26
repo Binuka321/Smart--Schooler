67686f3f35fe274be968fafc6f7765b55bfbdf3d
Dinuka Amarasekara <it22233226@my.sliit.lk>
2025-04-26 20:22:15 +0530

    Model file fixed

--- Skill-horizon/server/src/main/java/com/example/Skill/Horizon/Models/LearningPlan.java
+++ Skill-horizon/server/src/main/java/com/example/Skill/Horizon/Models/LearningPlan.java
@@ -17,8 +17,5 @@ public class LearningPlan {
     private List<String> items;
 }
 
-//This file defines what a LearningPlan is — kind of like a form template. 
-// Each plan has an id, a title, a subtitle, and a list of items (steps or goals). 
-// It’s stored in a MongoDB collection called "new_learning_plans". 
-// @DATA ---> Lombok takes care of the repetitive getter/setter code so you can focus on the real logic.
+//Updated the mdoel file
 
